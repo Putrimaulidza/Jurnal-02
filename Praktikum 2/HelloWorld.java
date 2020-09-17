8ad74f607a7de94bac11e0a49d3452e13115f2fb
Putrimaulidza <putrimaulidza693@gmail.com>
2020-09-17 12:05:29 +0700

    HelloWorld.java

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main(String[] args){
+		System.out.println("Nama : Putri Maulidza Yanti");
+		System.out.println("Nim : 6706203117");
+		System.out.println("Kelas : D3IF 44-04");
+	}
+}
+	
